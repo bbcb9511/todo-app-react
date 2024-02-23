38e8e3939e1f6aa08e6dc64ba8692d070a44eec2
murayama <bbcb9511@gmail.com>
2024-02-23 22:08:13 +0900

    クライアントとバックエンドの簡易動作確認済み

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let resString: any = '未設定';
+
+  fetch('http://localhost:8080/')
+    .then(response => {
+      resString = response;
+    } )
+    .catch(error => {
+      alert(error)
+    });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {resString}
     </div>
   );
 }
